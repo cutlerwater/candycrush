7ad45976e4c129dc2f8ce194299b7636ab1d337c
cutlerwater <cutlerwater2@live.com>
2022-01-27 14:11:45 -0500

    adding background changer

--- public/app2.js
+++ public/app2.js
@@ -9,3 +9,34 @@ floating_btn.addEventListener('click', () => {
 close_btn.addEventListener('click', () => {
 	social_panel_container.classList.remove('visible')
 });
+
+const countEl = document.getElementById('count');
+
+updateVisitCount();
+
+function updateVisitCount() {
+	fetch('https://api.countapi.xyz/update/florin-popcom/codepen/?amount=1')
+	.then(res => res.json())
+	.then(res => {
+		countEl.innerHTML = res.value;
+	})
+}
+
+const change = document.getElementById('change');
+const color = document.getElementById('color');
+const body = document.body;
+
+change.addEventListener('click', changeBG);
+
+function changeBG() {
+	const col1 = getRandomRGB();
+	const col2 = getRandomRGB();
+	const col3 = getRandomRGB();
+	const colorString = `rgb(${col1}, ${col2}, ${col3})`;
+	body.style.background = colorString;
+	color.innerText = colorString;
+}
+
+function getRandomRGB() {
+	return Math.floor(Math.random() * 256);
+}
